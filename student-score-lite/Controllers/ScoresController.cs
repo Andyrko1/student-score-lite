eb7102963cf456fc53e8e2ac1c8a94241a99ed93
Angel Hernandez <angelH98@outlook.com>
2021-09-19 11:56:12 -0600

    Controllers

---
+++ student-score-lite/Controllers/ScoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using student_score_lite.Models;
+
+namespace student_score_lite.Controllers
+{
+    public class ScoresController : Controller
+    {
+        private StudentScoreDBContext db = new StudentScoreDBContext();
+
+        // GET: Scores
+        public ActionResult Index()
+        {
+            return View(db.Score.ToList());
+        }
+
+        // GET: Scores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Score score = db.Score.Find(id);
+            if (score == null)
+            {
+                return HttpNotFound();
+            }
+            return View(score);
+        }
+
+        // GET: Scores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Scores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,grade,idTest,idStudent")] Score score)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Score.Add(score);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(score);
+        }
+
+        // GET: Scores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Score score = db.Score.Find(id);
+            if (score == null)
+            {
+                return HttpNotFound();
+            }
+            return View(score);
+        }
+
+        // POST: Scores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,grade,idTest,idStudent")] Score score)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(score).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(score);
+        }
+
+        // GET: Scores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Score score = db.Score.Find(id);
+            if (score == null)
+            {
+                return HttpNotFound();
+            }
+            return View(score);
+        }
+
+        // POST: Scores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Score score = db.Score.Find(id);
+            db.Score.Remove(score);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ student-score-lite/Models/StudentScoreDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace student_score_lite.Models
+{
+    public class StudentScoreDBContext : DbContext
+    {
+        public DbSet<Score> Score { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Test> Test { get; set; }
+    }
+}
